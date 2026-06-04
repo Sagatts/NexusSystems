ccbcbb330d5ca85a5931bc2de209c271e2c68ec7
envy <fvelizsilva@gmail.com>
2026-06-04 15:38:43 -0400

    Se agrega sidebar, css e íconos correspondientes al dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,27 +10,33 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    <div class="flex h-screen bg-gray-100">
+        
+        @include('components.sidebar')
+
+        <div class="flex-1 flex flex-col overflow-hidden">
+            
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @isset($header)
+            @if (isset($header))
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
-            @endisset
+            @endif
 
-            <!-- Page Content -->
-            <main>
+            <main class="flex-1 overflow-y-auto">
                 {{ $slot }}
             </main>
+            
         </div>
-    </body>
+    </div>
+</body>
 </html>
