fe86bd0d35008f7983c9ced166b480032ca2f0c5
Franco Andahur <francoandahur60@gmail.com>
2026-06-11 11:19:30 -0400

    se agrego filtro por categorias en productos

--- resources/views/admin/productos/index.blade.php
+++ resources/views/admin/productos/index.blade.php
@@ -35,6 +35,7 @@
 
                     <thead class="table-dark">
                         <tr>
+                            <th>ID</th>
                             <th>Código Barras</th>
                             <th>Nombre</th>
                             <th>Precio Neto</th>
@@ -56,112 +57,53 @@
 
     </div>
 
-    <!-- Modal Eliminar -->
-    <div class="modal fade" id="modalEliminar" tabindex="-1">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">
-                        Eliminar Producto
-                    </h5>
-                    <button
-                        type="button"
-                        class="btn-close"
-                        data-bs-dismiss="modal">
-                    </button>
-                </div>
-                <div class="modal-body">
-                    ¿Está seguro que desea eliminar el producto?
-                    <strong id="nombreProducto"></strong>
-                </div>
-                <div class="modal-footer">
-                    <button
-                        type="button"
-                        class="btn btn-secondary"
-                        data-bs-dismiss="modal">
-                        Cancelar
-                    </button>
-                    <form id="formEliminar" method="POST">
-                        @csrf
-                        @method('DELETE')
-
-                        <button type="submit" class="btn btn-danger">
-                            Sí, eliminar
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-
     @push('scripts')
     <script>
         $(document).ready(function () {
-            $('#tablaProductos').DataTable({
+
+            let tabla = $('#tablaProductos').DataTable({
+
                 processing: true,
                 serverSide: true,
-                ajax: "{{ route('admin.productos.datatable') }}",
+
+                ajax: {
+                    url: "{{ route('admin.productos.datatable') }}",
+                    data: function (d) {
+                        d.categoria = $('#filtro_categoria').val();
+                    }
+                },
+
                 language: {
                     url: '//cdn.datatables.net/plug-ins/1.13.7/i18n/es-ES.json',
-                    search: "Buscar por nombre:"
+                    search: "Buscar:"
                 },
+
                 columns: [
-                    {
-                        data: 'codigo_barras',
-                        name: 'codigo_barras'
-                    },
-                    {
-                        data: 'nombre',
-                        name: 'nombre'
-                    },
-                    {
-                        data: 'precio_neto',
-                        name: 'precio_neto'
-                    },
-                    {
-                        data: 'stock',
-                        name: 'stock'
-                    },
-                    {
-                        data: 'fecha_vencimiento',
-                        name: 'fecha_vencimiento'
-                    },
-                    {
-                        data: 'categoria',
-                        name: 'categoria'
-                    },
-                    {
-                        data: 'acciones',
-                        name: 'acciones',
-                        orderable: false,
-                        searchable: false
-                    }
-                ]
+                    { data: 'id', name: 'id' },
+                    { data: 'codigo_barras', name: 'codigo_barras' },
+                    { data: 'nombre', name: 'nombre' },
+                    { data: 'precio_neto', name: 'precio_neto' },
+                    { data: 'stock', name: 'stock' },
+                    { data: 'fecha_vencimiento', name: 'fecha_vencimiento' },
+                    { data: 'categoria', name: 'categoria' },
+                    { data: 'acciones', name: 'acciones', orderable: false, searchable: false }
+                ],
+
+
+                initComplete: function () {
+                    $('#filtro_categoria').removeClass('d-none');
+                    
+                    $('.dataTables_filter label').before($('#filtro_categoria'));
+                }
 
             });
 
+            $('#filtro_categoria').change(function(){
+                tabla.ajax.reload();
+            });
+
         });
-
     </script>
-
-    <script>
-
-        const rutaEliminarProducto = "{{ url('admin/productos') }}";
-
-        function abrirModalEliminar(id, nombre)
-        {
-            document.getElementById('nombreProducto').innerText = nombre;
-
-            document.getElementById('formEliminar').action =
-                rutaEliminarProducto + '/' + id;
-
-            new bootstrap.Modal(
-                document.getElementById('modalEliminar')
-            ).show();
-        }
-
-    </script>
-
     @endpush
 
 </x-app-layout>
