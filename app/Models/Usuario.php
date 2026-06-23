a376193780503db69b1463d930d742bcae5b8126
Fernando <fernando.arriagada.22@alumnos.uda.cl>
2026-06-23 17:05:06 -0400

    Se corrigio el boton eliminar de producto y usuario haciendo un softdelete

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -4,20 +4,19 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Notifications\CustomResetPasswordNotification;
+
+const DELETED_AT = 'deleted_at'; 
 
 class Usuario extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;  // ← agrega SoftDeletes
 
     protected $table = 'USUARIO';
-
     protected $primaryKey = 'rut';
-
     public $incrementing = false;
-
     protected $keyType = 'string';
-
     public $timestamps = false;
 
     protected $fillable = [
@@ -48,11 +47,9 @@ class Usuario extends Authenticatable
     {
         return $this->hasMany(Retiro::class, 'id_usuario', 'rut');
     }
-    
+
     public function sendPasswordResetNotification($token)
     {
-        $this->notify(
-            new CustomResetPasswordNotification($token)
-        );
+        $this->notify(new CustomResetPasswordNotification($token));
     }
 }
